a5e1f76214a9eb1d6f3612f591465605f8e88b41
Arman Avetisyan <arman@armans-mbp.home>
2019-11-17 13:11:31 -0500

    changes

--- linked_lists.py
+++ linked_lists.py
@@ -1,91 +1,92 @@
 class Person():
-    nextPerson = None #Person ()
+    nextPerson = None #Person()
     name = None #string
-    seatNumber = None #string
+    seatNumber = None #int
 
-    def __init__(self, name, seatNumber):
+    def __init__(self,name, seatNumber):
         self.name = name
         self.seatNumber = seatNumber
 
 class LinkedList:
-    head = None #Person()
-    
-    '''Add new person at the end of the list '''
+    head = None # Person()
+
+    ''' Add new person at the end of the list '''
     def append(self, person):
         if self.head == None:
             self.head = person
             return 
-
+        
         currentPerson = self.head
-     
         while currentPerson.nextPerson != None:
             currentPerson = currentPerson.nextPerson
         
         currentPerson.nextPerson = person
-    
-    '''Add Person at the begining of the list '''
-    def prepend(self, person):
+    ''' Add new person at the beginning of the list '''
+    def prepend(self,person):
         if self.head == None:
             self.head = person
             return
+        
         newHead = person
-        newHead.nextPerson = head
+        newHead.nextPerson = self.head
         self.head = newHead
     
-    '''Delete Person by name '''
-    def deletePerson(self, name):
-        if self.head == None:return
+    '''Delete Person by Name'''
+    def deletePerson(self,name):
+        if self.head == None: return
 
         if self.head.name == name:
-            self.head = head.nextPerson
-            return 
-
-        current = self.head
-        while current.nextPerson:
-            if current.nextPerson.name == name:
-                current.nextPerson = current.nextPerson.nextPerson
+            self.head = self.head.nextPerson
+            return
+        
+        currentPerson = self.head
+        while currentPerson.nextPerson != None:
+            if currentPerson.nextPerson.name == name:
+                currentPerson.nextPerson = currentPerson.nextPerson.nextPerson
                 return
+            currentPerson = currentPerson.nextPerson
 
-            current = current.nextPerson
-
-    '''Find person by name '''
+    ''' Find person by name '''
     def findPerson(self,name):
-        if self.head == None:return 
+        if self.head == None: return
 
         if self.head.name == name:
             return self.head
-        
+
         currentPerson = self.head
-        while currentPerson.nextPerson:
+        while currentPerson.nextPerson != None:
             if currentPerson.nextPerson.name == name:
                 return currentPerson.nextPerson
             
             currentPerson = currentPerson.nextPerson
-
+    
+    ''' Print all elements in the list '''
     def printList(self):
-        if self.head == None:print("List is empty")
+        if self.head == None: print("The list is empty")
 
         currentPerson = self.head
         print(currentPerson.name, currentPerson.seatNumber)
-        while currentPerson.nextPerson:
-            print(currentPerson.nextPerson.name,currentPerson.nextPerson.seatNumber)
+
+        while currentPerson.nextPerson != None:
+            print(currentPerson.nextPerson.name, currentPerson.nextPerson.seatNumber)
             currentPerson = currentPerson.nextPerson
-            
+
+
+
 
 myList = LinkedList()
-myList.append(Person(name="John", seatNumber=2))
-myList.append(Person(name="Julie", seatNumber=4))
-myList.append(Person(name="Andrew", seatNumber=5))
-myList.append(Person(name="Samuel", seatNumber=11))
+myList.append(Person(name="Julie", seatNumber=3))
+myList.append(Person(name="Simon", seatNumber=13))
+myList.append(Person(name="John", seatNumber=16))
+myList.append(Person(name="Smith", seatNumber=20))
 
-#print(myList.head.nextPerson.nextPerson.nextPerson.name, myList.head.nextPerson.nextPerson.nextPerson.seatNumber)
-#print(myList.findPerson("Andrew").seatNumber)
+#myList.printList()
+#print("=== Find Person ===")
+#print(myList.findPerson(name="John").seatNumber, myList.findPerson(name="John").name)
+
+myList.prepend(Person(name="Andrew", seatNumber=2))
 myList.printList()
-myList.deletePerson("Andrew")
-print("==================")
+
+print("==============")
+myList.deletePerson(name="Simon")
 myList.printList()
-#myList.deletePerson("Andrew")
-
-
-
-
